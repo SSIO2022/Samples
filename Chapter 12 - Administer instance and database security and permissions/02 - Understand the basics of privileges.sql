79e552894f3e4eab920bcbc06018cf6e21ee0140
william assaf non-MSFT <6641470+williamadba@users.noreply.github.com>
2022-11-06 06:46:32 -0800

    Update 02 - Understand the basics of privileges.sql

--- Chapter 12 - Administer instance and database security and permissions/02 - Understand the basics of privileges.sql
+++ Chapter 12 - Administer instance and database security and permissions/02 - Understand the basics of privileges.sql
@@ -12,26 +12,26 @@ The ON portion of the permission statement may be optional depending on whether 
 */
 
 
-GRANT EXECUTE TO [domain\katie.sql];
+GRANT EXECUTE TO [domain\kirby.sql];
 
 /*
 Hence, you may be granted access to read some data, for example in a schema named Sales, through multiple methods. You may also be denied via another path. GRANT and DENY oppose each other, with DENY taking precedence. To demonstrate, consider the following opposing GRANT and DENY statements run from an administrative account on the WideWorldImporters sample database:
 */
 
-GRANT SELECT on SCHEMA::Sales to [domain\katie.sql];
-DENY SELECT on OBJECT::Sales.InvoiceLines to [domain\katie.sql];
+GRANT SELECT on SCHEMA::Sales to [domain\kirby.sql];
+DENY SELECT on OBJECT::Sales.InvoiceLines to [domain\kirby.sql];
 
---Or you can grant [Domain\Katie_sql] access to insert, update, and delete data using:
+--Or you can grant [Domain\kirby.sql] access to insert, update, and delete data using:
 
-GRANT INSERT, UPDATE, DELETE on SCHEMA::Sales to [domain\katie.sql];
+GRANT INSERT, UPDATE, DELETE on SCHEMA::Sales to [domain\kirby.sql];
 
 --It doesn’t matter how many times you are granted access to a resource, DENY overrides it. You can delete the DENY using:
 
-REVOKE SELECT on OBJECT::Sales.Invoices to [domain\katie.sql];
+REVOKE SELECT on OBJECT::Sales.Invoices to [domain\kirby.sql];
 
 /*
 And then delete the original GRANT using the following statements (which can be condensed into one statement, if desired, they needn’t match the GRANT):
 */
 
-REVOKE SELECT on SCHEMA::Sales to [domain\katie.sql];
-REVOKE INSERT, UPDATE, DELETE on SCHEMA::Sales to [domain\katie.sql];
+REVOKE SELECT on SCHEMA::Sales to [domain\kirby.sql];
+REVOKE INSERT, UPDATE, DELETE on SCHEMA::Sales to [domain\kirby.sql];
