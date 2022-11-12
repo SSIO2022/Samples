5e9a8a6bb6e830e3bcba49f70b8efa8e483985b3
josh smith <josh@accitentionaldba.com>
2022-11-11 19:16:30 -0800

    Added script headers Also added trailing new lines where missing.

--- Chapter 04 - Install and configure SQL Server instances and features/04 - Install SQL from PowerShell.ps1
+++ Chapter 04 - Install and configure SQL Server instances and features/04 - Install SQL from PowerShell.ps1
@@ -1,1 +1,9 @@
+##############################################################################
+#
+#   SAMPLE SCRIPTS TO ACCOMPANY "SQL SERVER 2022 ADMINISTRATION INSIDE OUT"
+#
+#   © MICROSOFT PRESS
+#
+##############################################################################
+
 .\Setup.exe /ConfigurationFile=c:\install\SQL2019_basic.INI
