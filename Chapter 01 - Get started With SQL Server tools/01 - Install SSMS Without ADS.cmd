50513b98e573f2428c3ca03573567d3c3052041b
josh smith <josh@accitentionaldba.com>
2022-11-11 19:02:19 -0800

    Adding header to chapter 1 scripts Also added square brackets to SQL script.

--- Chapter 01 - Get started With SQL Server tools/01 - Install SSMS Without ADS.cmd
+++ Chapter 01 - Get started With SQL Server tools/01 - Install SSMS Without ADS.cmd
@@ -1,3 +1,10 @@
+REM ##############################################################################
+REM
+REM SAMPLE SCRIPTS TO ACCOMPANY "SQL SERVER 2022 ADMINISTRATION INSIDE OUT"
+REM
+REM © MICROSOFT PRESS
+REM
+REM ##############################################################################
 
 REM Use command line to perform a silent install of SSMS without installing ADS
 REM Navigate to the location of the SSMS Installer and then execute the code below
