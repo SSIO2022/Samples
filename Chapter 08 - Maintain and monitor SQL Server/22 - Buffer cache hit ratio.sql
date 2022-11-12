2c397525cbdea87a7f0990e27d26c8b6130341e1
josh smith <josh@accitentionaldba.com>
2022-11-11 20:09:50 -0800

    Formatting and Syntax error fixes

--- Chapter 08 - Maintain and monitor SQL Server/22 - Buffer cache hit ratio.sql
+++ Chapter 08 - Maintain and monitor SQL Server/22 - Buffer cache hit ratio.sql
@@ -6,15 +6,17 @@
 --
 --##############################################################################
 
-SELECT Time_Observed = SYSDATETIMEOFFSET()
-    , Buffer_Cache_Hit_Ratio = convert(INT, 100 * (
-            SELECT cntr_value = convert(DECIMAL(9, 1), cntr_value)
-            FROM sys.dm_os_performance_counters AS pc
-            WHERE pc.COUNTER_NAME = 'Buffer cache hit ratio'
-                AND pc.OBJECT_NAME LIKE ' % :Buffer Manager % '
-            ) / (
-            SELECT cntr_value = convert(DECIMAL(9, 1), cntr_value)
-            FROM sys.dm_os_performance_counters AS pc
-            WHERE pc.COUNTER_NAME = 'Buffer cache hit ratio base'
-                AND pc.OBJECT_NAME LIKE ' % :Buffer Manager % '
-            ));
+SELECT SYSDATETIMEOFFSET() AS [Time_Observed]
+	, CONVERT(DECIMAL(9, 1) 
+		,100 * (
+			SELECT CONVERT(DECIMAL(9, 1), [cntr_value]) AS [cntr_value]
+			FROM [sys].[dm_os_performance_counters] AS [pc]
+			WHERE [pc].[COUNTER_NAME] = 'Buffer cache hit ratio'
+				AND [pc].[OBJECT_NAME] LIKE '%:Buffer Manager%'
+			) / (
+			SELECT CONVERT(DECIMAL(9, 1), [cntr_value]) AS [cntr_value]
+			FROM [sys].[dm_os_performance_counters] AS [pc]
+			WHERE [pc].[COUNTER_NAME] = 'Buffer cache hit ratio base'
+				AND [pc].[OBJECT_NAME] LIKE '%:Buffer Manager%'
+			)
+		) AS [Buffer_Cache_Hit_Ratio];
