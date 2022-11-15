6cde0c96c6dca1db439d59564b3f35fb388b02c6
josh smith <josh@accitentionaldba.com>
2022-11-15 10:36:32 -0800

    Removed extra line returns

--- Chapter 06 - Provision and configure SQL Server databases/08 - T-SQL for needed for migrating database files.sql
+++ Chapter 06 - Provision and configure SQL Server databases/08 - T-SQL for needed for migrating database files.sql
@@ -12,13 +12,11 @@ ALTER DATABASE [database_name] SET OFFLINE WITH ROLLBACK IMMEDIATE;
 
 ALTER DATABASE [database_name] MODIFY FILE (
     NAME = logical_data_file_name
-    , FILENAME = 'location\physical_data_file_name.mdf'
-    );
+    , FILENAME = 'location\physical_data_file_name.mdf');
 
 ALTER DATABASE [database_name] MODIFY FILE (
     NAME = logical_log_file_name
-    , FILENAME = 'location\physical_log_file_name.ldf'
-    );
+    , FILENAME = 'location\physical_log_file_name.ldf');
 
 /* ensure database files have been moved prior to running the following: */
 ALTER DATABASE [database_name] SET ONLINE;
