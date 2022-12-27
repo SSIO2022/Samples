659593b4f1d885a3951753237c9e63811bd9c792
josh smith <josh@accitentionaldba.com>
2022-12-27 11:02:30 -0800

    Header and minor formatting changes.

--- Chapter 12 - Administer instance and database security and permissions/01 - List Server Principals.sql
+++ Chapter 12 - Administer instance and database security and permissions/01 - List Server Principals.sql
@@ -1,14 +1,20 @@
+/*
+##############################################################################
 
---Security Principals
+	SAMPLE SCRIPTS TO ACCOMPANY "SQL SERVER 2022 ADMINISTRATION INSIDE OUT"
 
---You can view the SID and principal_id for a login in the sys.server_principals view.
+	© 2022 MICROSOFT PRESS
 
-SELECT name, sid, principal_id
-FROM   sys.server_principals;
+##############################################################################
+*/
 
---List server principals
+/*  Security Principals
+    You can view the SID and principal_id for a login in the sys.server_principals view. */
 
-SELECT name, type_desc, is_disabled
-FROM    sys.server_principals
-ORDER BY type_desc;
+SELECT [name], [sid], [principal_id]
+FROM   [sys].[server_principals];
 
+-- List server principals
+SELECT [name], [type_desc], [is_disabled]
+FROM    [sys].[server_principals]
+ORDER BY [type_desc];
